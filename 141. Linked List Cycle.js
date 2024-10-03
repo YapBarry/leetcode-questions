f07d651d8e8b2af3b24b4fed1382fc5a7d02ae10
Barry Yap <yapweiminbarry@gmail.com>
2024-10-04 00:23:40 +0800

    updated code for 141. Linked List Cycle

--- 141. Linked List Cycle.js
+++ 141. Linked List Cycle.js
@@ -17,7 +17,7 @@ var hasCycle = function(head) {
     let slow = head;
     let fast = head;
     if(head == null){return false}
-    while(fast.next  && fast.next.next){
+    while(fast  && fast.next){
         slow = slow.next;
         fast = fast.next.next;
         if(slow == fast){
